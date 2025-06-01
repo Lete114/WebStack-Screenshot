0a7640ad318aa1af67c7323d15ef5b996468d1c0
Lete114 <me@imlete.cn>
2025-06-01 18:55:29 +0800

    chore: use latest webstack-screenshot

--- api/index.js
+++ api/index.js
@@ -1,1 +1,35 @@
-module.exports = require('webstack-screenshot/dist/src/serverless')
+import { serverless } from 'webstack-screenshot'
+import { readdirSync } from 'fs'
+
+export default (req, res) => {
+  console.log('process.env', process.env)
+
+  const fonts = process.env.WEBSTACK_SCREENSHOT_FONTS.split(',')
+    .filter(Boolean)
+  console.log('fonts', fonts)
+  try {
+    
+ const dir= readdirSync('/tmp/fonts')
+ console.log('dir',dir)
+  } catch (error) {
+    console.log('fonts error',error)
+    
+  }
+  try {
+    
+ const dir= readdirSync('/tmp/.fonts')
+ console.log('dir',dir)
+  } catch (error) {
+    console.log('.fonts error',error)
+    
+  }
+  try {
+    
+ const dir= readdirSync('/tmp/fonts-cache')
+ console.log('dir',dir)
+  } catch (error) {
+    console.log('fonts-cache error',error)
+    
+  }
+  return serverless(req, res)
+}
